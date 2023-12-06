f11e910d6cbd7d959d23388e252d7ecc7463b81c
yunbing_xu <yunbing_xu@qq.com>
2023-12-06 14:30:46 +0800

    fix: 测试工程头文件目录问题

--- cmake/config_test.cmake
+++ cmake/config_test.cmake
@@ -4,7 +4,9 @@ auto_sources(TEST_C_FILES   "*.c"   "RECURSE" "${CMAKE_CURRENT_SOURCE_DIR}/test"
 auto_sources(TEST_H_FILES   "*.h"   "RECURSE" "${CMAKE_CURRENT_SOURCE_DIR}/test")
 auto_sources(TEST_CPP_FILES "*.cpp" "RECURSE" "${CMAKE_CURRENT_SOURCE_DIR}/test")
 
-include_directories(${TEST_H_FILES})
+find_include_dir(TEST_INC_DIRS "${TEST_H_FILES}") 
+list(REMOVE_DUPLICATES TEST_INC_DIRS)
+include_directories(${TEST_INC_DIRS})
 
 if (MSVC)
     add_executable(${TEST_NAME} 
