ae7832cda7e68ab941a392e402fe345852777909
yunbing_xu <yunbing_xu@qq.com>
2023-12-25 14:57:30 +0800

    style: 格式优化

--- test/lib_test.c
+++ test/lib_test.c
@@ -3,9 +3,9 @@
 #include "lib_.h"
 
 
-int buf2int_test()
+int byte2int_test()
 {
-    SLOG_INFO("--- buf2int test start ---\n");
+    SLOG_INFO("--- byte2int test start ---\n");
 
     uint8_t buf[] = {0x12, 0x34, 0x56, 0x78};
     SLOG_DEBUG("input:");
@@ -17,7 +17,7 @@ int buf2int_test()
     n = (int)byte2int(buf, NULL, 4, UCL_LITTLE_ENDIAN);
     SLOG_DEBUG("little endien: n = 0x%08x", n);
 
-    SLOG_INFO("--- buf2int test done ---\n");
+    SLOG_INFO("--- byte2int test done ---\n");
 
     return 0;
 }
@@ -27,7 +27,7 @@ int lib_test()
 {
     SLOG_INFO("-- lib test start --\n");
 
-    buf2int_test();
+    byte2int_test();
 
     SLOG_INFO("-- lib test done --\n");
 
